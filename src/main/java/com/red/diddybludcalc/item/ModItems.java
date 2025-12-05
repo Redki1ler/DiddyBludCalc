cc064eca327ec2751665338ec129068f07c23fcc
_1red <redkiller1005@gmail.com>
2025-12-05 09:02:16 +0200

    Added Foreskin and Calculator

--- src/main/java/com/red/diddybludcalc/item/ModItems.java
+++ src/main/java/com/red/diddybludcalc/item/ModItems.java
@@ -10,7 +10,9 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
-    public static final Item  RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item((new Item.Settings())));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item((new Item.Settings())));
+    public static final Item FORESKIN = registerItem("raw_foreskin", new Item(new Item.Settings()));
+    public static final Item CALCULATOR = registerItem("calculator", new Item(new Item.Settings()));
 
 
     private static Item registerItem(String name, Item item){
@@ -23,6 +25,8 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(PINK_GARNET);
             entries.add(RAW_PINK_GARNET);
+            entries.add(FORESKIN);
+            entries.add(CALCULATOR);
         });
     }
 }
